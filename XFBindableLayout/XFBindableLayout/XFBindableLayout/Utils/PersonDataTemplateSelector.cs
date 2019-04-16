3c6214e216992e623fd6e01b89afb5fde0437529
Sebastian Jensen <apps@tsjdev-apps.de>
2019-04-16 07:15:07 +0200

    - add template selector

---
+++ XFBindableLayout/XFBindableLayout/XFBindableLayout/Utils/PersonDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+using XFBindableLayout.Models;
+
+namespace XFBindableLayout.Utils
+{
+    public class PersonDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FemaleDataTemplate { get; set; }
+        public DataTemplate MaleDataTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is Person person)
+            {
+                switch (person.Gender)
+                {
+                    case Gender.Female:
+                        return FemaleDataTemplate;
+                    case Gender.Male:
+                        return MaleDataTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
