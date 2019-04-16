4be07f5f3625980832a9f4028f11f53a1ecf4143
Sebastian Jensen <apps@tsjdev-apps.de>
2019-04-16 07:03:23 +0200

    - change layout of mainpage

--- XFBindableLayout/XFBindableLayout/XFBindableLayout/MainPage.xaml.cs
+++ XFBindableLayout/XFBindableLayout/XFBindableLayout/MainPage.xaml.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
+using XFBindableLayout.ViewModels;
 
 namespace XFBindableLayout
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ((MainPageViewModel)BindingContext).LoadPersonsCommand.Execute(null);
         }
     }
 }
