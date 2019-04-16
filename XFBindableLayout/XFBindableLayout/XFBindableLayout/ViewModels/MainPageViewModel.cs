ee890f6e8e04551dccc83f6829af7449400e8b2d
Sebastian Jensen <apps@tsjdev-apps.de>
2019-04-16 07:03:03 +0200

    - add mainpageviewmodel

---
+++ XFBindableLayout/XFBindableLayout/XFBindableLayout/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XFBindableLayout.Models;
+
+namespace XFBindableLayout.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private List<Person> _persons;
+        public List<Person> Persons
+        {
+            get => _persons;
+            set { _persons = value; OnPropertyChanged(); }
+        }
+
+        private ICommand _loadPersonsCommand;
+        public ICommand LoadPersonsCommand => _loadPersonsCommand ?? (_loadPersonsCommand = new Command(LoadPersons));
+
+        private void LoadPersons()
+        {
+            var persons = new List<Person>();
+            var ageRandom = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var age = ageRandom.Next(1, 100);
+                persons.Add(new Person { Name = $"Person {i + 1}", Age = age, Gender = age % 2 == 0 ? Gender.Female : Gender.Male });
+            }
+
+            Persons = persons;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+               
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
